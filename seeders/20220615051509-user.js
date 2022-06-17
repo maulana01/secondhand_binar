72c76f5854565194d1f02e26b355c03f37c3c56d
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-17 14:38:48 +0700

    menambahkan seeder pada table cities, category dan user

---
+++ seeders/20220615051509-user.js
@@ -0,0 +1,60 @@
+/** @format */
+
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          username: 'diahnas',
+          email: 'rodiah.akhfani@gmail.com',
+          password: 'inipassword',
+          name: 'Rodiah Akhfani Nasution',
+          gender: 'Perempuan',
+          address: 'Gg Mawar 3 Dsn 1 Galang Suka',
+          profile_picture: '',
+          phone_number: '085261774320',
+          role_id: '1',
+          city_id: '93',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          username: 'arara',
+          email: 'raraarara@gmail.com',
+          password: 'inipassword',
+          name: 'Rara Arara',
+          gender: 'Perempuan',
+          address: 'Gg Mawar 5 Dsn 5 Galang Suka',
+          profile_picture: '',
+          phone_number: '081360122285',
+          role_id: '2',
+          city_id: '93',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
