b923efab35473d26d4ccf02cbcdb5208c22c32e4
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-22 22:55:24 +0700

    memperbaiki typo

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
-const router = require("express").Router();
-const AuthCtl = require("../controllers/Auth");
-const auth_path = "/api/v1/auth";
+/** @format */
+
+const router = require('express').Router();
+const AuthCtl = require('../controllers/auth');
+const auth_path = '/api/v1/auth';
 
 router.post(`${auth_path}/signup`, AuthCtl.signup);
 router.post(`${auth_path}/login`, AuthCtl.login);
