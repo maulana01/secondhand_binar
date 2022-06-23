8a2c7c55c0efd1249c6beff021b39da525a40493
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-23 21:55:55 +0700

    memperbaiki error

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -5,22 +5,31 @@ const jwt = require('jsonwebtoken');
 module.exports = async (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    const error = new Error('Not authenticated.');
-    error.statusCode = 401;
-    throw error;
+    // const error = new Error('Not authenticated.');
+    // error.statusCode = 401;
+    // throw error;
+    return res.status(401).json({
+      error: 'Not authenticated.',
+    });
   }
   const token = authHeader.split(' ')[1];
   let decodedToken;
   try {
     decodedToken = await jwt.verify(token, 'supersecret');
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    // err.statusCode = 500;
+    // throw err;
+    return res.status(500).json({
+      error: err.message,
+    });
   }
   if (!decodedToken) {
-    const error = new Error('Not authenticated.');
-    error.statusCode = 401;
-    throw error;
+    // const error = new Error('Not authenticated.');
+    // error.statusCode = 401;
+    // throw error;
+    return res.status(401).json({
+      error: 'Not authenticated.',
+    });
   }
   req.userLoggedin = decodedToken;
   next();
