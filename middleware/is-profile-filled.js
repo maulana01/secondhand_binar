b2944e91ec198add589bb13b61c74d24239f2516
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-24 06:21:30 +0700

    memperbaiki error

--- middleware/is-profile-filled.js
+++ middleware/is-profile-filled.js
@@ -5,11 +5,7 @@ const { user: User } = require('../models');
 module.exports = async (req, res, next) => {
   if (req.userLoggedin) {
     const { product_name, product_desc, product_price, category_id, product_images_name } = req.body;
-    console.log('ini hasil product_name', product_name);
-    console.log('ini hasil product_desc', product_desc);
-    console.log('ini hasil product_price', product_price);
-    console.log('ini hasil category_id', category_id);
-    console.log('ini hasil product_images_name', product_images_name);
+    console.log('ini hasil body', req.body);
     const { userId } = req.userLoggedin;
     const user = await User.findOne({
       where: {
