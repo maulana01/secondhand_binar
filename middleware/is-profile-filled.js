9f6b0ce965fda6c811f2d685aa06217237eb7643
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-23 13:14:49 +0700

    memperbaiki error

--- middleware/is-profile-filled.js
+++ middleware/is-profile-filled.js
@@ -5,15 +5,9 @@ const { user: User } = require('../models');
 module.exports = async (req, res, next) => {
   const { userId } = req.userLoggedin;
   const user = await User.findByPk(userId);
-  console.log('ini hasil user', user);
-  console.log('ini address', user.dataValues.address);
-  console.log('ini address dalem', user.user.dataValues.address);
-  if (
-    user.dataValues.address == null ||
-    user.dataValues.phone_number == null ||
-    user.dataValues.city_id == null ||
-    user.dataValues.profile_picture == null
-  ) {
+  // console.log('ini hasil user', user);
+  // console.log('ini address', user.dataValues.address);
+  if (user.address == null || user.phone_number == null || user.city_id == null || user.profile_picture == null) {
     res.status(400).json({
       message: 'Please complete your profile first!',
     });
