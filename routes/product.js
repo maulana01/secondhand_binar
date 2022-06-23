d117bdc58248d9a31af16735edb3ee744c5b2292
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-23 12:24:47 +0700

    memperbaiki error

--- routes/product.js
+++ routes/product.js
@@ -4,8 +4,24 @@ const router = require('express').Router();
 const ProductCtl = require('../controllers/product');
 const product_path = '/api/v1/product';
 const IsAuth = require('../middleware/is-auth');
-const IsProfileFilled = require('../middleware/is-profile-filled');
+// const IsProfileFilled = require('../middleware/is-profile-filled');
 const UploadUtil = require('../utils/uploadProducts');
+
+function IsProfileFilled(req, res, next) {
+  const { userId } = req.userLoggedin;
+  User.findByPk(userId).then((user) => {
+    if (user.address == null || user.phone_number == null || user.city_id == null || user.profile_picture == null) {
+      // res.status(400).json({
+      //   message: 'Please complete your profile first!',
+      // });
+      const error = new Error('Please complete your profile first!');
+      error.statusCode = 400;
+      throw error;
+    } else {
+      next();
+    }
+  });
+}
 
 router.get(`${product_path}`, ProductCtl.getAllWithPaginationSortingFiltering);
 router.get(`${product_path}/:slug`, ProductCtl.getProductDetailBySlug);
