58a88fac338ec985969c178f59af820f72126fed
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-23 19:02:27 +0700

    memperbaiki error

--- middleware/is-profile-filled.js
+++ middleware/is-profile-filled.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   console.log('ini userId', userId);
   User.findByPk(userId)
     .then((user) => {
-      if (user.address == null || user.phone_number == null || user.city_id == null || user.profile_picture == null) {
+      if (user.address == null && user.phone_number == null && user.city_id == null && user.profile_picture == null) {
         res.status(400).json({
           message: 'Please complete your profile first!',
         });
