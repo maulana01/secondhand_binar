4c1634f774d99052d0cd3ff8c4b6d72e4c85785c
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-24 06:42:13 +0700

    memperbaiki error

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -6,12 +6,12 @@ module.exports = async (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
     console.log('ini req body authheader', req.body);
-    const error = new Error('Not authenticated.');
-    error.statusCode = 401;
-    throw error;
-    // return res.status(401).json({
-    //   error: 'Not authenticated.',
-    // });
+    // const error = new Error('Not authenticated.');
+    // error.statusCode = 401;
+    // throw error;
+    return res.status(401).json({
+      error: 'Not authenticated.',
+    });
   }
   const token = authHeader.split(' ')[1];
   let decodedToken;
@@ -19,20 +19,20 @@ module.exports = async (req, res, next) => {
     decodedToken = await jwt.verify(token, 'supersecret');
   } catch (err) {
     console.log('ini req body error 500', req.body);
-    err.statusCode = 500;
-    throw err;
-    // return res.status(500).json({
-    //   error: err.message,
-    // });
+    // err.statusCode = 500;
+    // throw err;
+    return res.status(500).json({
+      error: err,
+    });
   }
   if (!decodedToken) {
     console.log('ini req body decodedtoken', req.body);
-    const error = new Error('Not authenticated.');
-    error.statusCode = 401;
-    throw error;
-    // return res.status(401).json({
-    //   error: 'Not authenticated.',
-    // });
+    // const error = new Error('Not authenticated.');
+    // error.statusCode = 401;
+    // throw error;
+    return res.status(401).json({
+      error: 'Not authenticated.',
+    });
   }
   req.userLoggedin = decodedToken;
   next();
