ffab5bbff8f25928a2e90b97b1d7d4647639d264
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-07-04 14:25:19 +0700

    menambahkan middleware is-auth pada wishlist routes

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,9 +1,12 @@
+/** @format */
+
 const router = require('express').Router();
 const WishlistCtl = require('../controllers/wishlist');
 const wishlist_path = '/api/v1/wishlist';
+const IsAuth = require('../middleware/is-auth');
 
-router.get(`${wishlist_path}`, WishlistCtl.getAll);
-router.post(`${wishlist_path}`, WishlistCtl.create);
-router.delete(`${wishlist_path}/:id`, WishlistCtl.delete);
+router.get(`${wishlist_path}`, IsAuth, WishlistCtl.getAll);
+router.post(`${wishlist_path}`, IsAuth, WishlistCtl.create);
+router.delete(`${wishlist_path}/:id`, IsAuth, WishlistCtl.delete);
 
 module.exports = router;
