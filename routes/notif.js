b28bc78d699684daf200680a191d86ab64f70f06
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-07-13 13:12:43 +0700

    menambahkan endpoint get all notification

--- routes/notif.js
+++ routes/notif.js
@@ -1,13 +1,11 @@
-const router = require("express").Router();
-const NotificationCtl = require("../controllers/notification");
-const isAuth = require("../middleware/is-auth");
-const notification_path = "/api/v1/notification";
+/** @format */
+
+const router = require('express').Router();
+const NotificationCtl = require('../controllers/notification');
+const isAuth = require('../middleware/is-auth');
+const notification_path = '/api/v1/notification';
 
 router.get(`${notification_path}`, isAuth, NotificationCtl.getNotification);
-router.get(
-  `${notification_path}/all`,
-  isAuth,
-  NotificationCtl.getAllNotification
-);
+router.get(`${notification_path}/all`, NotificationCtl.getAllNotification);
 
 module.exports = router;
