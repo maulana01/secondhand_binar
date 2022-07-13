59fd00ff20aa6bd84fa5beb411aa664b73625b43
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-07-13 09:23:26 +0700

    memperbaiki typo

--- middleware/is-profile-filled.js
+++ middleware/is-profile-filled.js
@@ -6,7 +6,13 @@ module.exports = async (req, res, next) => {
   const { userId } = req.userLoggedin;
   const user = await User.findByPk(userId);
   // console.log('ini address', user.dataValues.address);
-  if (user.address == null || user.phone_number == null || user.city_id == null || user.profile_picture == null) {
+  if (
+    user.address == null ||
+    user.phone_number == null ||
+    user.city_id == null ||
+    user.profile_picture == null ||
+    user.profile_picture_path == null
+  ) {
     return res.status(403).json({
       message: 'Please complete your profile first!',
     });
