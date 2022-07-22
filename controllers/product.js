c1df127b49c106e4f4f3275de0d59a816ac29aba
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-07-22 17:55:19 +0700

    memperbaiki bug

--- controllers/product.js
+++ controllers/product.js
@@ -251,7 +251,7 @@ exports.getAllBySeller = async (req, res, next) => {
 
 exports.getProductDetailBySlug = (req, res, next) => {
   const slug = req.params.slug;
-  const authHeader = req.get('Authorization');
+  const authHeader = req.userLoggedin;
   console.log('ini authheader', authHeader);
   Product.findOne({
     where: {
